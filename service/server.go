a4132f1eb9323714dfbf043bf3f246a5f545a30a
Fabrizio Torelli <fabrizio.torelli@wipro.com>
2017-04-12 09:22:11 +0100

    Improved SSL CA Certificate Handshake

--- service/server.go
+++ service/server.go
@@ -29,13 +29,23 @@ func GateWayPortServer(config model.Configuration, sites []model.Site, waitGroup
 	go func(server *http.Server, config model.Configuration, waitGroup *sync.WaitGroup) {
 		if config.UseTLS {
 			if config.CACertFile != "" {
-				caCert, _ := ioutil.ReadFile(config.X509CertFile)
+				caCert, _ := ioutil.ReadFile(config.CACertFile)
 				caCertPool := x509.NewCertPool()
 				caCertPool.AppendCertsFromPEM(caCert)
 				server.TLSConfig = &tls.Config{
 					RootCAs: caCertPool,
 					ClientAuth: tls.RequireAndVerifyClientCert,
+					MinVersion:               tls.VersionTLS12,
+					CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+					PreferServerCipherSuites: true,
+					CipherSuites: []uint16{
+						tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+						tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+						tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+						tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+					},
 				}
+				server.TLSNextProto= make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0)
 			}
 			err = server.ListenAndServeTLS(config.X509CertFile, config.X509KeyFile)
 		} else {
